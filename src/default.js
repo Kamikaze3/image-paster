c77736932b9d911e3e3416804d53eb3801c65956
Frank Lyder Bredland <fl@nyggen.com>
2017-01-24 17:20:13 +0100

    Some simplification

--- src/default.js
+++ src/default.js
@@ -1,34 +1,31 @@
 document.addEventListener('paste', function(e){
 	if (e.clipboardData) {
-		console.log(e.clipboardData);
 		var items = e.clipboardData.items;
 		if (items) {
-			paste_event_support = true;
-			//access data directly
 			for (var i = 0; i < items.length; i++) {
+				console.log(i, items[i]);
 				if (items[i].type.indexOf("image") !== -1) {
-					//image
 					var blob = items[i].getAsFile();
 					var URLObj = window.URL || window.webkitURL;
 					var source = URLObj.createObjectURL(blob);
-					paste_createImage(source);
+
+					var pastedImage = new Image();
+					pastedImage.onload = function () {
+						var canvas = document.createElement('canvas');
+						document.body.appendChild(canvas);
+
+						canvas.width = pastedImage.width;
+						canvas.height = pastedImage.height;
+						canvas.style = "border:1px solid grey;";
+						canvas.getContext('2d').drawImage(pastedImage, 0, 0);
+					};
+					pastedImage.src = source;
+				}
+				else {
+					items[i].getAsString(console.log);
 				}
 			}
 			e.preventDefault();
 		}
 	}
 }, false);
-
-function paste_createImage(source) {
-	var pastedImage = new Image();
-	pastedImage.onload = function () {
-		var canvas = document.createElement('canvas');
-		document.body.appendChild(canvas);
-
-		canvas.width = pastedImage.width;
-		canvas.height = pastedImage.height;
-		canvas.style = "border:1px solid grey;";
-		canvas.getContext('2d').drawImage(pastedImage, 0, 0);
-	};
-	pastedImage.src = source;
-};
