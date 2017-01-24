6f12da83c8a88305cd54e4af16ce0017c19728b6
Frank Lyder Bredland <fl@nyggen.com>
2017-01-24 16:56:42 +0100

    Small backend for uploading files

--- src/index.php
+++ src/index.php
@@ -1,3 +1,26 @@
 <?php
 
-echo file_get_contents('index.html');
+// config
+$fileUploadFolder = realpath(__DIR__ . '/uploads') . DIRECTORY_SEPARATOR;
+
+#if(!file_exists($fileUploadFolder))
+#    mkdir($fileUploadFolder, 0777, true);
+
+// Routing
+switch($_SERVER['REQUEST_URI'])
+{
+	case 'fileupload':
+        $file = $_FILES['file'];
+
+        #$originalFilenameExtension = pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION);
+        $hash = sha1(file_get_contents($file));
+
+        $newFilename = $fileUploadFolder . $hash;
+        $file->move($fileUploadFolder, $hash);
+
+        echo "{id:'$hash'}";
+		break;
+	default:
+		echo file_get_contents('index.html');
+}
+
