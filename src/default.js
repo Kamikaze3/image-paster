fb321e93145eec2e7b89afc874a4d1e70cfe5dfe
Frank Lyder Bredland <fl@nyggen.com>
2017-01-24 17:37:23 +0100

    Uploading works

--- src/default.js
+++ src/default.js
@@ -1,14 +1,16 @@
+var resourceLink = "/file";
+
 document.addEventListener('paste', function(e){
 	if (e.clipboardData) {
 		var items = e.clipboardData.items;
 		if (items) {
 			for (var i = 0; i < items.length; i++) {
-				console.log(i, items[i]);
 				if (items[i].type.indexOf("image") !== -1) {
 					var blob = items[i].getAsFile();
 					var URLObj = window.URL || window.webkitURL;
 					var source = URLObj.createObjectURL(blob);
 
+					// Create new image
 					var pastedImage = new Image();
 					pastedImage.onload = function () {
 						var canvas = document.createElement('canvas');
@@ -20,9 +22,21 @@ document.addEventListener('paste', function(e){
 						canvas.getContext('2d').drawImage(pastedImage, 0, 0);
 					};
 					pastedImage.src = source;
-				}
-				else {
-					items[i].getAsString(console.log);
+
+					// Upload the image
+					var formData = new FormData(); 
+					formData.append('file', blob);
+
+					var xhr = new XMLHttpRequest();
+
+					xhr.onreadystatechange = function() {
+						if (xhr.readyState == 4)
+							console.log(xhr.responseText);
+					};
+
+					xhr.open('POST', resourceLink, true);
+					xhr.setRequestHeader('X_FILENAME', blob.name);
+					xhr.send(formData);
 				}
 			}
 			e.preventDefault();
--- src/index.php
+++ src/index.php
@@ -1,25 +1,27 @@
 <?php
 
 // config
-$fileUploadFolder = realpath(__DIR__ . '/uploads') . DIRECTORY_SEPARATOR;
+$fileUploadFolder = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'uploads';
 
-#if(!file_exists($fileUploadFolder))
-#    mkdir($fileUploadFolder, 0777, true);
+if(!file_exists($fileUploadFolder))
+    mkdir($fileUploadFolder, 0777, true);
+
+// PS! realpath does not work on paths that does not exist
+$fileUploadFolder = realpath($fileUploadFolder);
 
 // Routing
 switch($_SERVER['REQUEST_URI'])
 {
-	case 'fileupload':
+	case '/file':
         $file = $_FILES['file'];
+        $hash = sha1_file($file['tmp_name']);
 
-        #$originalFilenameExtension = pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION);
-        $hash = sha1(file_get_contents($file));
-
-        $newFilename = $fileUploadFolder . $hash;
-        $file->move($fileUploadFolder, $hash);
+        $newFilename = $fileUploadFolder . DIRECTORY_SEPARATOR . $hash;
+        move_uploaded_file($file['tmp_name'], $newFilename);
 
         echo "{id:'$hash'}";
 		break;
+
 	default:
 		echo file_get_contents('index.html');
 }
